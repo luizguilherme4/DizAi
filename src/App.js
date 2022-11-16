8ac52065643c5f66baafe0c9041da604f961b1a1
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2022-11-16 20:59:23 -0300

    finalizando aulas

--- src/App.js
+++ src/App.js
@@ -1,76 +1,20 @@
-import './styles/App.css';
-import './styles/PostForm.css';
-import './styles/Feed.css';
+import { BrowserRouter, Link } from 'react-router-dom';
 
-import userIcon from './images/user.svg';
-import paperPlaneIcon from './images/paper-plane.svg';
-import clockIcon from './images/clock.svg';
-import emptyFolderIcon from './images/empty-folder.svg';
+import AppRoutes from './Routes';
+
+import './styles/App.css';
 
 export default function App() {
-    const posts = [
-        {
-            id: Math.random(),
-            content: 'Conteúdo do post',
-            userName: 'Luiz',
-            publishedAt: new Date(),
-        }
-    ]
-
     return (
         <div className="wrapper">
-            <form className='post-form' onSubmit={() => alert('Formulário submetido!')}>
-                <input placeholder='Escreva uma nova história...' />
+            <BrowserRouter>
+                <nav>
+                    <Link to='/'>Meu feed</Link>
+                    <Link to='/most-viewed'>Mais vistos</Link>
+                </nav>
 
-                <div>
-                    <img src={userIcon} alt="User" />
-                    <input placeholder='Digite seu nome...' />
-                    <button type='submit'>
-                        <img src={paperPlaneIcon} alt="Paper Plane" />
-                        Publicar
-                    </button>
-                </div>
-            </form>
-
-            <main>
-                {posts.length == 0 && (
-                    <div className='feed-status'>
-                        <img src={emptyFolderIcon} alt="Empty folder" />
-                        <h1>Não encontramos nada</h1>
-                        <h2>
-                            Parece que você e seus amigos não postaram nada. Comece a escrever uma nova história!
-                        </h2>
-                    </div>
-                )}
-
-                {posts.length > 0 && (
-                    <>
-                        <header>
-                            <h1>Seu Feed</h1>
-                            <h2>Acompanhe o que seus amigos estão pensando em tempo real</h2>
-                        </header>
-
-                        <section className='feed'>
-                            {posts.map((post) => (
-                                <article key={post.id}>
-                                    <p>{post.content}</p>
-                                    <footer>
-                                        <div className='user-details'>
-                                            <img src={userIcon} alt='User' />
-                                            <strong>{post.userName}</strong>
-                                        </div>
-                                        <div className='time'>
-                                            <img src={clockIcon} alt='Clock' />
-                                            <span>Publicado em {post.publishedAt.toLocaleDateString('pt-br')}</span>
-                                        </div>
-                                    </footer>
-                                </article>
-                            ))}
-                        </section>
-                    </>
-                )}
-
-            </main>
+                <AppRoutes />
+            </BrowserRouter>
         </div>
     )
 }
